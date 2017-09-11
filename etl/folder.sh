0fe8389c18596605800b0626ba202fa19380fa81
parisni <nicolas.paris@riseup.net>
2017-09-11 18:22:24 +0200

    improve mapping generation

--- etl/folder.sh
+++ etl/folder.sh
@@ -12,7 +12,7 @@ do # Whitespace-safe but not recursive.
 	file="${BASH_REMATCH[2]}"
 	mkdir -p "$folder/$file/"
 	echo 'omop_table,omop_column,mimic_table,mimic_column,comments' > "$folder/$file/mapping.csv"
-	grep -e "^|" "$f" |sed '/^Field/ d'|sed '/^:--/ d'|sed "s/'/''/g" |sed "s/|\([a-z_]*\).*|.*|.*|.*|/$file,\1,,,/g" >> "$folder/$file/mapping.csv"
+	grep -e "^|" "$f" |sed '/^Field/ d'|sed '/^:--/ d'|sed "s/'/''/g" |sed "s/|[ ]\{0,10\}\([a-z_]*\).*|.*|.*|.*|/$file,\1,,,/g" >> "$folder/$file/mapping.csv"
 	rm "$f"
     else
         echo "$f doesn't match" >&2 # this could get noisy if there are a lot of non-matching files
