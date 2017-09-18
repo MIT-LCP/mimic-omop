a50f1f113a1e23c921e85e353bb239984e6135e5
parisni <nicolas.paris@riseup.net>
2017-09-18 17:57:08 +0200

    added omop type & mimic type columns

--- etl/folder.sh
+++ etl/folder.sh
@@ -11,8 +11,8 @@ do # Whitespace-safe but not recursive.
 	folder="${BASH_REMATCH[1]}"
 	file="${BASH_REMATCH[2]}"
 	mkdir -p "$folder/$file/"
-	echo 'omop_table,omop_column,omop_required,mimic_table,mimic_column,comments' > "$folder/$file/mapping.csv"
-	grep -e "^|" "$f" |sed '/^Field/ d'|sed '/^:--/ d'|sed "s/'/''/g" |sed "s/|[ ]\{0,10\}\([a-z_]*\)[ ]\{0,10\}|[ ]\{0,10\}\([A-Za-z]*\)[ ]\{0,10\}|.*|.*|/$file,\1,\2,,,/g" >> "$folder/$file/mapping.csv"
+	echo 'omop_table,omop_column,omop_required,omop_type,mimic_table,mimic_column,mimic_type,comments' > "$folder/$file/mapping.csv"
+	grep -e "^|" "$f" |sed '/^Field/ d'|sed '/^:--/ d'|sed "s/'/''/g" |sed "s/|[ ]\{0,10\}\([a-z_]*\).*|[ ]\{0,10\}\([A-Za-z]*\).*|[ ]\{0,10\}\([A-Za-z()0-9 ]*\).*|.*|/$file,\1,\2,\3,,,,/g" >> "$folder/$file/mapping.csv"
     else
         echo "$f doesn't match" >&2 # this could get noisy if there are a lot of non-matching files
     fi
