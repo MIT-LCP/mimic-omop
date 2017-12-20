4493f31e560392e2a8c8fc8b92f52f9ea829bec6
Nicolas Paris <nicolas.paris@ahph.fr>
2017-12-20 21:38:05 +0100

    introduces the PERSON etl

--- etl/StandardizedClinicalDataTables/PERSON/etl_template.sql
+++ etl/StandardizedClinicalDataTables/PERSON/etl_template.sql
@@ -1,6 +1,8 @@
- WITH patients AS (SELECT subject_id as person_id, CASE WHEN gender ='F' THEN 8532 WHEN GENDER = 'M' THEN 8507 ELSE NULL END as gender_concept_id, extract(year FROM dob) as year_of_birth, extract(month FROM dob) as month_of_birth, extract(day FROM dob) as day_of_birth, dob as birth_datetime, gender as gender_source_value FROM mimic.patients),
+ WITH patients AS (SELECT mimic_id as person_id, CASE WHEN gender ='F' THEN 8532 WHEN GENDER = 'M' THEN 8507 ELSE NULL END as gender_concept_id, extract(year FROM dob) as year_of_birth, extract(month FROM dob) as month_of_birth, extract(day FROM dob) as day_of_birth, dob as birth_datetime, gender as gender_source_value FROM mimic.patients),
+gcpt_ethnicity_to_concept AS (SELECT race_concept_id as race_concept_id, ethnicity_concept_id as ethnicity_concept_id FROM mimic.gcpt_ethnicity_to_concept),
 admissions AS (SELECT ethnicity as ethnicity_source_value FROM mimic.admissions) 
- INSERT INTO omop.PERSON (person_id, gender_concept_id, year_of_birth, month_of_birth, day_of_birth, birth_datetime, gender_source_value, ethnicity_source_value)
- SELECT patients.person_id, patients.gender_concept_id, patients.year_of_birth, patients.month_of_birth, patients.day_of_birth, patients.birth_datetime, patients.gender_source_value, admissions.ethnicity_source_value 
+ INSERT INTO omop.PERSON (person_id, gender_concept_id, year_of_birth, month_of_birth, day_of_birth, birth_datetime, gender_source_value, race_concept_id, ethnicity_concept_id, ethnicity_source_value)
+ SELECT patients.person_id, patients.gender_concept_id, patients.year_of_birth, patients.month_of_birth, patients.day_of_birth, patients.birth_datetime, gcpt_ethnicity_to_concept.race_concept_id, gcpt_ethnicity_to_concept.ethnicity_concept_id, patients.gender_source_value, admissions.ethnicity_source_value 
 FROM patients
+ LEFT JOIN gcpt_ethnicity_to_concept ON ()
  LEFT JOIN admissions ON () 
