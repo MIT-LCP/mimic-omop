6da7e79ad5fcb73c967188d258308a60395bd760
Nicolas Paris <nicolas.paris@aphp.fr>
2017-12-21 23:19:30 +0100

    add a global etl script

---
+++ etl/etl.sql
@@ -0,0 +1,5 @@
+\i StandardizedHealthSystemDataTables/CARE_SITE/etl.sql
+\i StandardizedClinicalDataTables/PERSON/etl.sql
+\i StandardizedClinicalDataTables/DEATH/etl.sql
+\i StandardizedClinicalDataTables/VISIT_OCCURRENCE/etl.sql
+\i StandardizedClinicalDataTables/VISIT_DETAIL/etl.sql
