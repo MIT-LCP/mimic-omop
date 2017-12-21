0e943633fb7eedb1d03edf08aa6ca3d79da9beaa
parisni <nicolas.paris@riseup.net>
2017-12-21 20:06:16 +0100

    add new columns to omop death

--- omop/build-omop/postgresql/mimic-omop-add-column.sql
+++ omop/build-omop/postgresql/mimic-omop-add-column.sql
@@ -1,3 +1,11 @@
---ALTER TABLE omop.death ADD COLUMN visit_detail_id BIGINT;
-COMMENT ON COLUMN omop.death.visit_detail_id             IS 'A foreign key to the visit in the VISIT_DETAIL table during where the death occured';
+ALTER TABLE omop.death ADD COLUMN visit_detail_id BIGINT;
+COMMENT ON COLUMN omop.death.visit_detail_id             IS '[CONTRIB] A foreign key to the visit in the VISIT_DETAIL table during where the death occured';
 
+ALTER TABLE omop.death ADD COLUMN visit_occurrence_id BIGINT;
+COMMENT ON COLUMN omop.death.visit_occurrence_id             IS '[CONTRIB] A foreign key to the visit in the VISIT_OCCURRENCE table during where the death occured';
+
+ALTER TABLE omop.death ADD COLUMN death_visit_detail_delay double precision;
+COMMENT ON COLUMN omop.death.death_visit_detail_delay             IS '[CONTRIB] Difference between deathtime and visit_start_datetime of VISIT_DETAIL table';
+
+ALTER TABLE omop.death ADD COLUMN death_visit_occurrence_delay double precision;
+COMMENT ON COLUMN omop.death.death_visit_occurrence_delay      IS '[CONTRIB] Difference between deathtime and visit_start_datetime of VISIT_OCCURRENCE table';
