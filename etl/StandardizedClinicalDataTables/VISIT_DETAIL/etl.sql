c6a9bb6c2fcb3db5b086b26d80a57a345fee40f3
Nicolas Paris <nicolas.paris@aphp.fr>
2017-12-30 19:29:17 +0100

    add mimic.services information into visit_detail
    again, some strange behavior such hadm_id = 155841 some transfertime
    match to transfers intime, while other does not match.
    Should be investigated why

--- etl/StandardizedClinicalDataTables/VISIT_DETAIL/etl.sql
+++ etl/StandardizedClinicalDataTables/VISIT_DETAIL/etl.sql
@@ -7,7 +7,42 @@
 "transfers_bed" AS ( select t1.*, sum(group_flag) over (partition by hadm_id order by intime) as grp from ( select *, case when lag(value) over (partition by hadm_id order by intime) = value then null else 1 end as group_flag from bed_tmp) t1),
 "transfers_no_bed" as(SELECT  distinct on (hadm_id, grp) transfers_bed.*,  min(intime) OVER(PARTITION BY hadm_id, grp) as intime_real, max(outtime) OVER(PARTITION BY hadm_id, grp) as outtime_real FROM transfers_bed ORDER BY hadm_id, grp, intime),
 "transfers_light" AS (SELECT subject_id, hadm_id, curr_careunit, mimic_id as visit_detail_id, 9201 as visit_detail_concept_id, transfers_no_bed.intime_real::date as visit_start_date, intime_real as visit_start_datetime, outtime_real::date as visit_end_date, outtime_real as visit_end_datetime, 44818518 as visit_type_concept_id, callout_discharge_to_source_value  as discharge_to_source_value, prev_careunit as admitting_source_value, LEAD(mimic_id) OVER (PARTITION BY hadm_id ORDER BY transfers_no_bed.intime_real ASC) as later_visit_detail_id, LAG(mimic_id) OVER (PARTITION BY hadm_id ORDER BY transfers_no_bed.intime_real ASC) as preceding_visit_detail_id, discharge_delay FROM transfers_no_bed),
-"transfers" AS ( SELECT t10.subject_id , t10.hadm_id , t10.visit_detail_id , t10.visit_detail_concept_id , t10.visit_start_date , t10.visit_start_datetime , t10.visit_end_date , t10.visit_end_datetime , t10.visit_type_concept_id , CASE WHEN t10.preceding_visit_detail_id IS NULL THEN NULL ELSE coalesce(t9.curr_careunit,'UNKNOWN') END as admitting_source_value , coalesce(t10.curr_careunit,t9.discharge_to_source_value,'UNKNOWN') as curr_careunit , CASE WHEN t10.later_visit_detail_id IS NULL THEN NULL ELSE coalesce(t11.curr_careunit, t10.discharge_to_source_value,'UNKNOWN') END as discharge_to_source_value , t10.later_visit_detail_id , t10.preceding_visit_detail_id , t10.discharge_delay FROM transfers_light as t10 LEFT JOIN transfers_light as t9 ON (t10.preceding_visit_detail_id = t9.visit_detail_id) LEFT JOIN transfers_light as t11 ON (t10.later_visit_detail_id     = t11.visit_detail_id)),
+"transfers" AS (
+                          SELECT t10.subject_id
+                               , t10.hadm_id
+                               , t10.visit_detail_id
+                               , t10.visit_detail_concept_id
+                               , t10.visit_start_date
+                               , t10.visit_start_datetime
+                               , t10.visit_end_date
+                               , t10.visit_end_datetime
+                               , t10.visit_type_concept_id
+                               , CASE WHEN t10.preceding_visit_detail_id IS NULL THEN NULL ELSE coalesce(t9.curr_careunit,'UNKNOWN') END as admitting_source_value
+                               , coalesce( t10.curr_careunit, t9.discharge_to_source_value,t10bis.curr_careunit,'UNKNOWN') as curr_careunit
+                               , CASE WHEN t10.later_visit_detail_id IS NULL THEN NULL ELSE coalesce(t11.curr_careunit, t10.discharge_to_source_value,'UNKNOWN') END as discharge_to_source_value
+                               , t10.later_visit_detail_id
+                               , t10.preceding_visit_detail_id
+                               , t10.discharge_delay
+                            FROM transfers_light as t10
+                          LEFT 
+                          JOIN (
+                                        SELECT hadm_id
+                                             , transfertime
+                                             , curr_service as curr_careunit
+                          FROM mimic.services
+                               ) as t10bis
+                            ON (
+                                       t10bis.hadm_id      = t10.hadm_id
+                           AND t10bis.transfertime = t10.visit_start_datetime
+                               )
+                            LEFT 
+                            JOIN transfers_light as t9
+                              ON (t10.preceding_visit_detail_id = t9.visit_detail_id)
+                            LEFT 
+                            JOIN transfers_light as t11
+              ON (t10.later_visit_detail_id     = t11.visit_detail_id)
+                 )
+               , 
 "patients" AS (SELECT subject_id, mimic_id as person_id FROM mimic.patients),
 "gcpt_care_site" AS (SELECT care_site_name, mimic_id as care_site_id FROM mimic.gcpt_care_site),
 "gcpt_visit_detail_source_to_concept" AS (SELECT visit_source_value, visit_source_concept_id FROM mimic.gcpt_visit_detail_source_to_concept UNION ALL SELECT care_site_name as visit_source_value, place_of_service_concept_id as visit_source_concept_id FROM mimic.gcpt_care_site),
