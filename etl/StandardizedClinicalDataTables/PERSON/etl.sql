161f43768986d8e4f041b3a4ea9c03fd57191026
parisni <nicolas.paris@riseup.net>
2017-12-31 01:01:10 +0100

    change ethnicity to the first one
    this is related to #15

--- etl/StandardizedClinicalDataTables/PERSON/etl.sql
+++ etl/StandardizedClinicalDataTables/PERSON/etl.sql
@@ -1,6 +1,6 @@
 WITH patients AS (SELECT subject_id, mimic_id as person_id, CASE WHEN gender ='F' THEN 8532 WHEN GENDER = 'M' THEN 8507 ELSE NULL END as gender_concept_id, extract(year FROM dob) as year_of_birth, extract(month FROM dob) as month_of_birth, extract(day FROM dob) as day_of_birth, dob as birth_datetime, gender as gender_source_value FROM mimic.patients),
 gcpt_ethnicity_to_concept AS (SELECT ethnicity, race_concept_id as race_concept_id, ethnicity_concept_id as ethnicity_concept_id FROM mimic.gcpt_ethnicity_to_concept),
-admissions AS (SELECT DISTINCT ON (subject_id) subject_id, ethnicity as ethnicity_source_value FROM mimic.admissions ORDER BY subject_id, admittime DESC) 
+admissions AS (SELECT DISTINCT ON (subject_id) subject_id, ethnicity as ethnicity_source_value FROM mimic.admissions ORDER BY subject_id, admittime ASC) 
  INSERT INTO omop.PERSON (person_id, gender_concept_id, year_of_birth, month_of_birth, day_of_birth, birth_datetime, gender_source_value, race_concept_id, ethnicity_concept_id, ethnicity_source_value)
  SELECT patients.person_id
       , patients.gender_concept_id
