143e23c63e95ab25e3c49448e8c1107849b0aa9c
Nicolas Paris <nicolas.paris@aphp.fr>
2017-12-31 18:09:38 +0100

    document death and improve code
    this is related to #18

--- etl/StandardizedClinicalDataTables/DEATH/etl.sql
+++ etl/StandardizedClinicalDataTables/DEATH/etl.sql
@@ -1,4 +1,9 @@
- WITH patients AS (SELECT mimic_id as person_id, dod as death_date, dod as death_datetime, CASE WHEN dod_hosp IS NULL THEN 261 ELSE 38003569 END as death_type_concept_id FROM mimic.patients WHERE dod IS NOT NULL) 
+WITH 
+"death_adm" AS (SELECT patients.mimic_id as person_id, least(deathtime, dischtime) as death_datetime, 38003569 as death_type_concept_id FROM mimic.admissions LEFT JOIN mimic.patients USING (subject_id) WHERE deathtime IS NOT NULL),
+"death_ssn" AS (SELECT mimic_id as person_id, dod_ssn as death_datetime, 261 as death_type_concept_id FROM mimic.patients LEFT JOIN death_adm ON (mimic_id = person_id) WHERE dod_ssn IS NOT NULL AND death_adm.person_id IS NULL) 
  INSERT INTO omop.DEATH (person_id, death_date, death_datetime, death_type_concept_id)
- SELECT patients.person_id, patients.death_date, patients.death_datetime, patients.death_type_concept_id 
-FROM patients ;
+ SELECT person_id, death_datetime::date, death_datetime, death_type_concept_id 
+ FROM  death_adm
+UNION ALL
+ SELECT person_id, death_datetime::date, death_datetime, death_type_concept_id 
+ FROM  death_ssn;
