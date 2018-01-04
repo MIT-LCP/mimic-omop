fddd6d0096f065ebb3ba98575bdbb0efc3415f37
Nicolas Paris <nicolas.paris@aphp.fr>
2018-01-04 23:14:41 +0100

    Introduce COHORT etl
    Also the example here adds 6 types of cohorts, based on severe/shock
    kind of sepsis and demonstrate the feasibility to describe cohort based
    on different granularity ranging from patient to admissions to stays to
    hourly ond so on.

---
+++ etl/StandardizedDerivedElements/COHORT/etl.sql
@@ -0,0 +1,49 @@
+INSERT INTO omop.cohort 
+(
+  cohort_definition_id 
+, subject_id          
+, cohort_start_date   
+, cohort_end_date     
+
+)
+SELECT
+1 -- angus severe
+, mimic_id , admittime , dischtime
+FROM sepsis.hadm_deliberation
+JOIN mimic.admissions USING (hadm_id)
+WHERE angus = 1
+UNION
+SELECT
+2 -- angus shock
+, mimic_id , admittime , dischtime
+FROM sepsis.hadm_deliberation
+JOIN mimic.admissions USING (hadm_id)
+WHERE angus = 2
+UNION
+SELECT
+3 -- accp_severe
+, mimic_id , admittime , dischtime
+FROM sepsis.hadm_deliberation
+JOIN mimic.admissions USING (hadm_id)
+WHERE accp = 1
+UNION
+SELECT
+4 -- accp_shock
+, mimic_id , admittime , dischtime
+FROM sepsis.hadm_deliberation
+JOIN mimic.admissions USING (hadm_id)
+WHERE accp = 2
+UNION
+SELECT
+5 -- sepsis severe
+, mimic_id , admittime , dischtime
+FROM sepsis.hadm_deliberation
+JOIN mimic.admissions USING (hadm_id)
+WHERE sepsis3 = 1
+UNION
+SELECT
+6 -- sepsis shock
+, mimic_id , admittime , dischtime
+FROM sepsis.hadm_deliberation
+JOIN mimic.admissions USING (hadm_id)
+WHERE sepsis3 = 2
--- etl/StandardizedDerivedElements/COHORT_ATTRIBUTE/etl.sql
+++ etl/StandardizedDerivedElements/COHORT_ATTRIBUTE/etl.sql
@@ -31,16 +31,6 @@ VALUES
 )
 ,
 (         3
-        , 'accp simple'
-        , 'jerome description'
-        , 0
-        , ''
-       -- , 'visit_occurrence'
-        , 0
-        , now()
-)
-,
-(         4
         , 'accp severe'
         , 'jerome description'
         , 0
@@ -50,7 +40,7 @@ VALUES
         , now()
 )
 ,
-(         5
+(         4
         , 'accp shock'
         , 'jerome description'
         , 0
@@ -60,7 +50,7 @@ VALUES
         , now()
 )
 ,
-(         6
+(         5
         , 'sepsis3 severe'
         , 'jerome description'
         , 0
@@ -70,7 +60,7 @@ VALUES
         , now()
 )
 ,
-(         7
+(         6
         , 'sepsis3 shock'
         , 'jerome description'
         , 0
