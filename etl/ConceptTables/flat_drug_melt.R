3ddd7f98a8a32b4c054f094c7cdee382c8102e98
Nicolas Paris <nicolas.paris@aphp.fr>
2018-01-06 23:33:07 +0100

    Add some mapping to inputevent drug file

--- etl/ConceptTables/flat_drug_melt.R
+++ etl/ConceptTables/flat_drug_melt.R
@@ -2,8 +2,8 @@
 # thanks again to him
 library(data.table)
 DT <- fread("../extras/inputevents_drug_flat.csv")
-DT <- melt(DT, id.vars = c("label"),
+DT <- melt(DT, id.vars = c("label","concept_id"),
                 measure.vars = c("itemid_cv_1", "item_cv_2", "item_cv_3","itemid_mv_1","item_mv_2"))
 DT[,variable:=NULL]
 setnames(DT,c('label','value'),c("label","itemid"))
-write.table(DT[!is.na(itemid)],"../extras/google/concept/inputevents_drug.csv",sep=",",col.names=TRUE, row.names=FALSE,quote=TRUE)
+write.table(DT[!is.na(itemid)],"../extras/google/concept/inputevents_drug_to_concept.csv",sep=",",col.names=TRUE, row.names=FALSE,quote=TRUE)
