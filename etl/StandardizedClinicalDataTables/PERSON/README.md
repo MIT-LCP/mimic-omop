3ea956f3b62f77d4c776822add3dd6b955e3162c
aparrot <adrien.parrot@caramail.fr>
2018-01-09 10:15:11 +0100

    Add SQL queries to describe person table Add check_etl.sql to check etl of person table     - Need to install pgTAP : http://pgtap.org/

--- etl/StandardizedClinicalDataTables/PERSON/README.md
+++ etl/StandardizedClinicalDataTables/PERSON/README.md
@@ -1,16 +1,91 @@
+# URL to CommonDataModel
+- https://github.com/OHDSI/CommonDataModel/wiki/PERSON
+
 # Source Tables
 
-## `mimic.patients`
+## mimic.patients
 
 - information about death are mooved to `omop.death` table
 
-## `mimic.admissions`
+## mimic.admissions
 
 - the ethicity comes from admissions, the first recorded value is put in the omop.person table 
 
 # Lookup Tables
 
-## `gcpt_ethnicity_to_concept`
+## gcpt_ethnicity_to_concept
 
 - this maps the mimic ethnicity to omop ethnicity
 - this has been made by google
+
+# Examples
+
+## Standard queries from http://cdmqueries.omop.org/person
+
+``` sql
+-- Number of patients in the dataset
+SELECT COUNT(person_id) AS num_persons_count
+FROM person;
+```
+
+``` sql
+-- Number of patients grouped by gender
+SELECT  person.GENDER_CONCEPT_ID,
+        concept.CONCEPT_NAME AS ethnicity_name,
+        COUNT(person.person_ID) AS num_persons_count
+FROM person
+INNER JOIN concept ON person.GENDER_concept_id = concept.CONCEPT_ID
+GROUP BY person.GENDER_CONCEPT_ID, concept.CONCEPT_NAME;
+```
+
+``` sql
+-- Number of patients grouped by race
+SELECT  person.RACE_CONCEPT_ID,
+        concept.CONCEPT_NAME AS race_name,
+        COUNT(person.person_ID) AS num_persons_count
+FROM person
+INNER JOIN concept ON person.RACE_CONCEPT_ID = concept.CONCEPT_ID
+GROUP BY person.RACE_CONCEPT_ID, concept.CONCEPT_NAME;
+```
+
+``` sql
+-- Number of patients grouped by ethnicity
+SELECT  person.ETHNICITY_CONCEPT_ID,
+        concept.CONCEPT_NAME AS ethnicity_name,
+        COUNT(person.person_ID) AS num_persons_count
+FROM person
+INNER JOIN concept ON person.ETHNICITY_CONCEPT_ID = concept.CONCEPT_ID
+GROUP BY person.ETHNICITY_CONCEPT_ID, concept.CONCEPT_NAME;
+```
+
+``` sql
+-- Distribution of year of birth
+SELECT percentile_25
+     , median
+     , percentile_75
+     , MIN( year_of_birth  )    AS minimum
+     , MAX( year_of_birth  )    AS maximum
+     , CAST(AVG( year_of_birth  ) AS INTEGER)   AS mean
+     , STDDEV( year_of_birth  ) AS stddev
+FROM
+(SELECT MAX( CASE WHEN( percentile = 1  ) THEN year_of_birth END  ) AS percentile_25
+      , MAX( CASE WHEN( percentile = 2  ) THEN year_of_birth END  ) AS median
+      , MAX( CASE WHEN( percentile = 3  ) THEN year_of_birth END  ) AS percentile_75
+  FROM
+     ( SELECT counter.year_of_birth, counter.births
+            , FLOOR( CAST( SUM( births  ) OVER( ORDER BY year_of_birth ROWS UNBOUNDED PRECEDING  ) AS DECIMAL  )
+                   / CAST( SUM( births  ) OVER( ORDER BY year_of_birth ROWS BETWEEN UNBOUNDED PRECEDING
+                                                                      AND UNBOUNDED FOLLOWING  )  AS DECIMAL  )
+                   * 4
+                    ) + 1
+        as percentile
+        FROM
+           ( SELECT year_of_birth, count(*) AS births
+              FROM person
+              GROUP BY year_of_birth
+            ) as counter
+     ) as p
+   WHERE percentile <= 3
+) as percentile_table, person
+GROUP BY percentile_25, median, percentile_75;
+```
---
+++ etl/StandardizedClinicalDataTables/PERSON/check_etl.sql
@@ -0,0 +1,112 @@
+
+-- -----------------------------------------------------------------------------
+-- File created - January-8-2018
+-- ----------------------------------------------------------------------------
+
+-- --------------------------------------------------
+-- Need to install pgTAP
+-- http://pgtap.org/
+-- --------------------------------------------------
+
+BEGIN;
+SELECT plan (3);
+-- 1. number patients checker
+SELECT results_eq
+(
+'
+SELECT COUNT(*) AS num_persons_count
+FROM omop.person;
+'
+,
+'
+SELECT COUNT(*) AS num_persons_count
+FROM patients;
+' 
+);
+
+-- 2. race checker
+SELECT results_eq
+(
+'
+SELECT COUNT(person.person_ID) AS num_persons_count
+FROM omop.person as person
+INNER JOIN omop.concept as concept ON person.gender_concept_id = concept.CONCEPT_ID
+GROUP BY person.gender_CONCEPT_ID, concept.CONCEPT_NAME
+ORDER BY num_persons_count DESC;
+'
+,
+'
+SELECT  COUNT(gender) AS num_persons_count
+FROM patients
+GROUP BY gender
+ORDER BY num_persons_count DESC;
+'
+);
+
+-- 3. distribution checker
+SELECT results_eq
+(
+'
+SELECT percentile_25
+       , median
+       , percentile_75
+       , MIN( extract(year from dob  ))    AS minimum
+       , MAX( extract(year from dob  )) AS maximum
+       , CAST(AVG( extract(year from dob  )) AS INTEGER)   AS mean
+       , FLOOR(STDDEV( extract(year from dob  ))) AS stddev
+  FROM
+  (SELECT MAX( CASE WHEN( percentile = 1  ) THEN year_of_birth END  ) AS percentile_25
+        , MAX( CASE WHEN( percentile = 2  ) THEN year_of_birth END  ) AS median
+        , MAX( CASE WHEN( percentile = 3  ) THEN year_of_birth END  ) AS percentile_75
+    FROM
+       ( SELECT counter.year_of_birth, counter.births
+              , FLOOR( CAST( SUM( births  ) OVER( ORDER BY year_of_birth ROWS UNBOUNDED PRECEDING  ) AS DECIMAL  )
+                     / CAST( SUM( births  ) OVER( ORDER BY year_of_birth ROWS BETWEEN UNBOUNDED PRECEDING
+                                                                        AND UNBOUNDED FOLLOWING  )  AS DECIMAL  )
+                     * 4
+                      ) + 1
+          as percentile
+          FROM
+             ( SELECT extract(year from dob) as year_of_birth, count(*) AS births
+                FROM patients
+                GROUP BY extract(year from dob)
+              ) as counter
+       ) as p
+     WHERE percentile <= 3
+    ) as percentile_table, patients
+  GROUP BY percentile_25, median, percentile_75;
+'
+,
+'
+SELECT percentile_25
+       , median
+       , percentile_75
+       , MIN(extract(year from birth_datetime) )    AS minimum
+       , MAX(extract(year from birth_datetime ))    AS maximum
+       , CAST(AVG(extract(year from birth_datetime)) AS INTEGER)   AS mean
+       , FLOOR(STDDEV( extract(year from birth_datetime)  )) AS stddev
+  FROM
+  (SELECT MAX( CASE WHEN( percentile = 1  ) THEN year_of_birth END  ) AS percentile_25
+        , MAX( CASE WHEN( percentile = 2  ) THEN year_of_birth END  ) AS median
+        , MAX( CASE WHEN( percentile = 3  ) THEN year_of_birth END  ) AS percentile_75
+    FROM
+       ( SELECT counter.year_of_birth, counter.births
+           , FLOOR( CAST( SUM( births  ) OVER( ORDER BY year_of_birth ROWS UNBOUNDED PRECEDING  ) AS DECIMAL )
+		/ CAST( SUM( births  ) OVER( ORDER BY year_of_birth ROWS BETWEEN UNBOUNDED PRECEDING
+                                                            AND UNBOUNDED FOLLOWING  )  AS DECIMAL )
+                     * 4
+                      ) + 1
+          as percentile
+          FROM
+             ( SELECT extract(year from birth_datetime) as year_of_birth, count(*) AS births
+                FROM omop.person as person
+                GROUP BY extract(year from birth_datetime)
+              ) as counter
+       ) as p
+     WHERE percentile <= 3
+    ) as percentile_table, omop.person
+  GROUP BY percentile_25, median, percentile_75;
+'
+);
+SELECT * FROM finish();
+ROLLBACK;
