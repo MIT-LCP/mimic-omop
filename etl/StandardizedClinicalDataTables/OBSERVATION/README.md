74e95edf0f3a318cba8e1995996794004c93208e
aparrot <adrien.parrot@caramail.fr>
2018-01-09 15:44:19 +0100

    Add SQL queries to describe observation table Add check_etl.sql to check etl of observation table     - Need to install pgTAP : http://pgtap.org/

--- etl/StandardizedClinicalDataTables/OBSERVATION/README.md
+++ etl/StandardizedClinicalDataTables/OBSERVATION/README.md
@@ -1,3 +1,6 @@
+# URL to CommonDataModel
+- https://github.com/OHDSI/CommonDataModel/wiki/OBSERVATION
+
 # Source Tables
 
 ## datetimeevents
@@ -19,6 +22,32 @@
 # Lookup Tables
 
 - `gcpt_insurance_to_concept`
+https://github.com/MIT-LCP/mimic-omop/blob/master/extras/google/concept/insurance_to_concept.csvi
+
 - `gcpt_ethnicity_to_concept`
+https://github.com/MIT-LCP/mimic-omop/blob/master/extras/google/concept/ethnicity_to_concept.csv
+
 - `gcpt_religion_to_concept`
+https://github.com/MIT-LCP/mimic-omop/blob/master/extras/google/concept/religion_to_concept.csv
+
 - `gcpt_marital_status_to_concept`
+https://github.com/MIT-LCP/mimic-omop/blob/master/extras/google/concept/marital_status_to_concept.csv
+
+# Examples
+``` sql
+-- Repartition of different concept in observation table
+SELECT observation_concept_id, concept_name, count(1) 
+FROM observation 
+JOIN concept on observation_concept_id = concept_id 
+group by 1, 2 order by 3 desc;
+```
+ observation_concept_id |                   concept_name                    | count
+------------------------+---------------------------------------------------+--------
+                      0 | No matching concept                               | 160011
+                4085802 | Referred by nurse                                 |  10342
+                4296248 | Cost containment                                  |    278
+               44803968 | Ethnicity - National Public Health Classification |    127
+               46235654 | Primary insurance                                 |    127
+               40766231 | Marital status                                    |    101
+               40758030 | Language.preferred                                |     73
+                4052017 | Religious affiliation                             |     67
---
+++ etl/StandardizedClinicalDataTables/OBSERVATION/check_etl.sql
@@ -0,0 +1,117 @@
+
+-- -----------------------------------------------------------------------------
+-- File created - January-9-2018
+-- ----------------------------------------------------------------------------
+
+-- --------------------------------------------------
+-- Need to install pgTAP
+-- http://pgtap.org/
+-- --------------------------------------------------
+
+BEGIN;
+SELECT plan ( 5 );
+
+-- 1. religion checker -- 4052017
+SELECT results_eq
+(
+'
+SELECT CAST(value_as_string AS TEXT) as religion
+     , COUNT(1) 
+FROM omop.observation 
+WHERE observation_concept_id = 4052017
+GROUP BY 1 ORDER BY 2, 1 DESC;
+'
+
+,
+'
+SELECT cast(religion as text), count(1) 
+FROM admissions 
+WHERE religion is not null 
+and relegion != 'OTHER' and relegion != 'NOT SPECIFIED' and religion != 'UNOBTAINABLE'
+GROUP BY 1 ORDER BY 2, 1 desc;
+' 
+);
+
+-- 2. language checker -- 40758030
+SELECT results_eq
+(
+'
+SELECT CAST(value_as_string AS TEXT) as language
+     , COUNT(1) 
+FROM omop.observation 
+WHERE observation_concept_id = 40758030
+GROUP BY 1 ORDER BY 2, 1 DESC;
+'
+
+,
+'
+SELECT cast(language as TEXT), count(1) 
+FROM admissions 
+WHERE language is not null 
+GROUP BY 1 ORDER BY 2, 1 desc;
+' 
+);
+
+-- 3. marital checker -- 40766231
+SELECT results_eq
+(
+'
+SELECT CAST(value_as_string AS TEXT) as marital_status
+     , COUNT(1) 
+FROM omop.observation 
+WHERE observation_concept_id = 40766231
+GROUP BY 1 ORDER BY 2, 1 DESC;
+'
+
+,
+'
+SELECT CAST(marital_status AS TEXT), count(1) 
+FROM admissions 
+WHERE marital_status is not null 
+GROUP BY 1 ORDER BY 2, 1 desc;
+' 
+);
+
+-- 4. insurance checker -- 46235654
+SELECT results_eq
+(
+'
+SELECT CAST(value_as_string AS TEXT) as insurance
+     , COUNT(1) 
+FROM omop.observation 
+WHERE observation_concept_id = 46235654
+GROUP BY 1 ORDER BY 2, 1 DESC;
+'
+
+,
+'
+SELECT CAST(insurance AS TEXT), count(1) 
+FROM admissions 
+WHERE insurance is not null 
+GROUP BY 1 ORDER BY 2, 1 desc;
+' 
+);
+
+
+-- 5. ethnicity checker -- 44803968
+SELECT results_eq
+(
+'
+SELECT CAST(value_as_string AS TEXT) as ethnicity
+     , COUNT(1) 
+FROM omop.observation 
+WHERE observation_concept_id =  44803968
+GROUP BY 1 ORDER BY 2, 1 DESC;
+'
+
+,
+'
+SELECT CAST(ethnicity AS TEXT), count(1) 
+FROM admissions 
+WHERE ethnicity is not null 
+GROUP BY 1 ORDER BY 2, 1 desc;
+' 
+);
+
+SELECT * FROM finish();
+ROLLBACK;
