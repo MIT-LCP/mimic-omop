90bf55a6cfdf6c2e228f563c588dd7ebe2bf7b71
Nicolas Paris <nicolas.paris@aphp.fr>
2018-01-14 18:51:41 +0100

    Improve export strategy

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ export:
 	find etl/Result/ -name "*.gz" -delete &&\
 	find etl/Result/ -name "*.tar" -delete &&\
 	find etl/Result/ -name "*.sql" -delete &&\
-	psql -h localhost  -d mimic postgres  -f etl/export_mimic_omop.sql &&\
+	psql -h $(HOST_OMOP)  -d mimic postgres  -f etl/export_mimic_omop.sql &&\
 	cp etl/import_mimic_omop.sql etl/Result/ &&\
 	cp omop/build-omop/postgresql/* etl/Result/ &&\
 	tar -cf etl/Result/mimic-omop.tar etl/Result/
--- etl/StandardizedClinicalDataTables/NOTE_NLP/load.sql
+++ etl/StandardizedClinicalDataTables/NOTE_NLP/load.sql
@@ -1,3 +1,22 @@
+--- useful for note_nlp etl integration
+CREATE TABLE omop.tmp_note_nlp_concept
+(       
+  section_code integer
+, category_code integer
+, section_text text
+);
+
+CREATE TABLE omop.tmp_note_nlp 
+(       
+  row_id integer
+, section_index integer
+, section_count integer
+, section_code integer 
+, section_begin integer
+, section_end integer 
+, section_text text
+);
+
 TRUNCATE TABLE omop.tmp_note_nlp;
 \copy omop.tmp_note_nlp FROM PROGRAM 'gzip -dc ~/git/mimic-omop/extras/private/fait_doc_section_mimic.csv.gz' CSV DELIMITER ';' NULL '' QUOTE '"' ESCAPE E'\\';
 TRUNCATE TABLE omop.tmp_note_nlp_concept;
