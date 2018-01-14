1f3b2ac120d9686555d790901e41da4a374fcc4c
aparrot <adrien.parrot@caramail.fr>
2018-01-14 21:40:09 +0100

    Add chect_test.sql in MEASUREMENT TABLE

--- etl/StandardizedClinicalDataTables/MEASUREMENT/check_etl.sql
+++ etl/StandardizedClinicalDataTables/MEASUREMENT/check_etl.sql
@@ -1,8 +1,7 @@
 BEGIN;
 
 SELECT plan(3);
-
--- 1.checker global distribution labevents globale(2)
+-- 1.checker global distribution labevents
 SELECT results_eq
 (
 '
@@ -31,7 +30,7 @@ Where concept_code IN
         from labevents join d_labitems using (itemid)
         group by itemid order by count(itemid) desc
   )
-AND value_as_number is not null and operator_concept_id is null
+AND value_as_number is not null and operator_concept_id = 4172703
 GROUP BY concept_code
 ORDER BY concept_code, count(measurement_source_concept_id) desc;
 '
@@ -53,7 +52,7 @@ where itemid IN
   )
 AND valuenum is not null
 GROUP BY pat.mimic_id, itemid
-ORDER BY pat.mimic_id desc, itemid desc, count(itemid) desc;
+ORDER BY pat.mimic_id desc, itemid desc, count(itemid) desc limit 1000;
 '
 ,
 '
@@ -66,9 +65,9 @@ Where concept_code IN
         from labevents join d_labitems using (itemid)
         group by itemid order by count(itemid) desc
   )
-AND value_as_number is not null and operator_concept_id is null
+AND value_as_number is not null and operator_concept_id  = 4172703
 GROUP BY person_id, concept_code
-ORDER BY person_id desc, concept_code desc, count(measurement_source_concept_id);
+ORDER BY person_id desc, concept_code desc, count(measurement_source_concept_id) limit 1000;
 '
 );
 
@@ -103,6 +102,47 @@ GROUP BY concept_code
 ORDER BY concept_code, count(measurement_source_concept_id) desc;
 '
 );
+
+
+-- 4.repartition des microorganismes
+SELECT results_eq
+(
+'
+SELECT org_name::TEXT, count(1) 
+FROM (
+	SELECT distinct on (hadm_id, spec_type_desc, org_name, coalesce( charttime, chartdate)) org_name--, count(org_name)
+	from microbiologyevents where org_name is not null
+     ) tmp
+GROUP BY org_name ORDER BY 2, 1 desc;
+'
+,
+'
+SELECT value_source_value::TEXT, count(1) FROM omop.measurement
+WHERE measurement_type_concept_id = 2000000007
+And value_as_concept_id is distinct from 9189
+Group by value_source_value order by 2, 1 desc;
+'
+);
+
+-- 5. examen negatif
+SELECT results_eq
+(
+'
+SELECT count(*)
+FROM (
+        SELECT distinct on (hadm_id, spec_itemid, coalesce (charttime, chartdate)) *--, count(org_name)
+        from microbiologyevents where org_name is null
+) tmp;
+'
+,
+'
+
+SELECT count(*)
+FROM omop.measurement 
+where measurement_type_concept_id = 2000000007 and value_as_concept_id = 9189;
+'
+);
+
 
 SELECT * from finish();
 ROLLBACK;
