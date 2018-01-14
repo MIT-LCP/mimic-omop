f569590602c2714bc3a9b1efee1cae6bb9c42a17
Nicolas Paris <nicolas.paris@aphp.fr>
2018-01-14 13:53:48 +0100

    Fix person race
    ethnicity is aborded,
    race is the first desscribed mimic admission ethnicity
    
    select count(1), concept_name from omop.person join omop.concept on race_concept_id = concept_id group by concept_name
    mimic-# ;
     count |          concept_name
    -------+---------------------------------
         3 | African American
         3 | Asian
         1 | European
         1 | Hispanic, color unknown
         4 | History of chronic lung disease
         3 | Other ethnic non-mixed
         1 | Portuguese
        16 | Unknown racial group
         1 | Vietnamese
        67 | White

--- etl/StandardizedClinicalDataTables/PERSON/etl.sql
+++ etl/StandardizedClinicalDataTables/PERSON/etl.sql
@@ -1,21 +1,27 @@
-WITH patients AS (SELECT subject_id, mimic_id as person_id, CASE WHEN gender ='F' THEN 8532 WHEN GENDER = 'M' THEN 8507 ELSE NULL END as gender_concept_id, extract(year FROM dob) as year_of_birth, extract(month FROM dob) as month_of_birth, extract(day FROM dob) as day_of_birth, dob as birth_datetime, gender as gender_source_value FROM patients),
-gcpt_ethnicity_to_concept AS (SELECT ethnicity, race_concept_id as race_concept_id, ethnicity_concept_id as ethnicity_concept_id FROM gcpt_ethnicity_to_concept),
-admissions AS (SELECT DISTINCT ON (subject_id) subject_id, ethnicity as ethnicity_source_value FROM admissions ORDER BY subject_id, admittime ASC) 
- INSERT INTO omop.PERSON (person_id, gender_concept_id, year_of_birth, month_of_birth, day_of_birth, birth_datetime, gender_source_value, race_concept_id, ethnicity_concept_id, ethnicity_source_value)
- SELECT patients.person_id
-      , patients.gender_concept_id
-      , patients.year_of_birth
-      , patients.month_of_birth
-      , patients.day_of_birth
-      , patients.birth_datetime
-      , patients.gender_source_value
-      , gcpt_ethnicity_to_concept.race_concept_id
-      , gcpt_ethnicity_to_concept.ethnicity_concept_id
-      , admissions.ethnicity_source_value
+WITH 
+"patients" AS (SELECT subject_id, mimic_id as person_id, CASE WHEN gender ='F' THEN 8532 WHEN GENDER = 'M' THEN 8507 ELSE NULL END as gender_concept_id, extract(year FROM dob) as year_of_birth, extract(month FROM dob) as month_of_birth, extract(day FROM dob) as day_of_birth, dob as birth_datetime, gender as gender_source_value FROM patients),
+"gcpt_ethnicity_to_concept" AS (SELECT ethnicity, race_concept_id as race_concept_id, ethnicity_concept_id as ethnicity_concept_id FROM gcpt_ethnicity_to_concept),
+"admissions" AS (SELECT DISTINCT ON (subject_id) subject_id, min(ethnicity) OVER(PARTITION BY subject_id ORDER BY admittime ASC) as race_source_value FROM admissions) 
+ INSERT INTO omop.PERSON 
+ SELECT 
+  patients.person_id                   
+, patients.gender_concept_id           
+, patients.year_of_birth               
+, patients.month_of_birth              
+, patients.day_of_birth                
+, patients.birth_datetime              
+, gcpt_ethnicity_to_concept.race_concept_id             
+, 0 as ethnicity_concept_id        
+, null::integer location_id                 
+, null::integer provider_id                 
+, null::integer care_site_id                
+, null::text person_source_value         
+, patients.gender_source_value         
+, null::integer gender_source_concept_id    
+, admissions.race_source_value           
+, null::integer race_source_concept_id      
+, null::text ethnicity_source_value      
+, null::integer ethnicity_source_concept_id 
    FROM patients
-   LEFT 
-   JOIN admissions
-  USING (subject_id)
-   LEFT 
-   JOIN gcpt_ethnicity_to_concept
-     ON (admissions.ethnicity_source_value = gcpt_ethnicity_to_concept.ethnicity);
+   LEFT JOIN admissions USING (subject_id)
+   LEFT JOIN gcpt_ethnicity_to_concept ON (admissions.race_source_value = gcpt_ethnicity_to_concept.ethnicity);
