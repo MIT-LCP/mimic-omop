2dd9b1939c0aaef26c4fd144bb19649b660a7dba
parisni <nicolas.paris@riseup.net>
2018-01-18 23:26:10 +0100

    Last run

--- etl/StandardizedClinicalDataTables/MEASUREMENT/etl.sql
+++ etl/StandardizedClinicalDataTables/MEASUREMENT/etl.sql
@@ -533,7 +533,7 @@ with
 	SELECT
 	  nextval('mimic_id_seq') as measurement_id
 	, person_id
-	, coalesce(measurement_concept_id, 0) as measurement_concept_id --not yet mapped
+	, coalesce(measurement_concept_id, 0) as measurement_concept_id -- mapped
 	, charttime::date as measurement_date
 	, charttime::timestamp as measurement_datetime
 	, 45754907 as measurement_type_concept_id --derived value
@@ -659,3 +659,61 @@ OR -- middle
 (is_last IS FALSE AND is_first IS FALSE AND row_to_insert.measurement_datetime > visit_detail_assign.visit_start_datetime AND row_to_insert.measurement_datetime <= visit_detail_assign.visit_end_datetime)
 )
 ;
+
+-- Derived values from noteevents
+
+with
+"patients" AS (SELECT mimic_id AS person_id, subject_id FROM patients),
+"admissions" AS (SELECT mimic_id AS visit_occurrence_id, hadm_id FROM admissions),
+"gcpt_derived_to_concept" as (select measurement_source_value, itemid, mimic_id as measurement_source_concept_id, concept_id as measurement_concept_id from gcpt_derived_to_concept),
+"row_to_insert" as (
+	SELECT
+	  nextval('mimic_id_seq') as measurement_id
+	, person_id
+	, coalesce(measurement_concept_id, 0) as measurement_concept_id -- mapped
+	, charttime::date as measurement_date
+	, charttime::timestamp as measurement_datetime
+	, 45754907 as measurement_type_concept_id --derived value
+	, CASE WHEN exact_value IS NOT NULL THEN 4172703 --=
+               WHEN inf_egal_value  IS NOT NULL THEN  4171756   --<
+               WHEN sup_egal_value IS NOT NULL THEN 4172704   END  -->
+          as operator_concept_id 
+	, coalesce(exact_value, inf_egal_value, sup_egal_value) as value_as_number
+	, null::integer as value_as_concept_id
+	, 8554 as unit_concept_id -- percent
+	, null::numeric as range_low
+	, null::numeric as range_high
+	, null::integer as provider_id
+	, visit_occurrence_id
+	, gcpt_derived_to_concept.measurement_source_value
+	, gcpt_derived_to_concept.measurement_source_concept_id
+	, '%' as unit_source_value
+	, null::text as value_source_value
+	FROM gcpt_derived_fevg
+	JOIN patients using(subject_id)
+	left join admissions using(hadm_id)
+	left join gcpt_derived_to_concept on 'LVEF from noteevents' = measurement_source_value
+        WHERE coalesce(exact_value, inf_egal_value, sup_egal_value) IS NOT NULL
+)
+INSERT INTO omop.measurement
+SELECT
+  row_to_insert.measurement_id
+, row_to_insert.person_id
+, row_to_insert.measurement_concept_id
+, row_to_insert.measurement_date
+, row_to_insert.measurement_datetime
+, row_to_insert.measurement_type_concept_id
+, row_to_insert.operator_concept_id
+, row_to_insert.value_as_number
+, row_to_insert.value_as_concept_id
+, row_to_insert.unit_concept_id
+, row_to_insert.range_low
+, row_to_insert.range_high
+, row_to_insert.provider_id
+, row_to_insert.visit_occurrence_id
+, null as visit_detail_id
+, row_to_insert.measurement_source_value
+, row_to_insert.measurement_source_concept_id
+, row_to_insert.unit_source_value
+, row_to_insert.value_source_value
+FROM row_to_insert;
