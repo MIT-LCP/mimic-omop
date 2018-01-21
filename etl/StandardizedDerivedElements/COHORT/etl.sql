a278536eeaaff66f6e8cb29163051844cae331fb
parisni <nicolas.paris@riseup.net>
2018-01-21 15:04:48 +0100

    Improve various detail

--- etl/StandardizedDerivedElements/COHORT/etl.sql
+++ etl/StandardizedDerivedElements/COHORT/etl.sql
@@ -10,40 +10,40 @@ SELECT
 1 -- angus severe
 , mimic_id , admittime , dischtime
 FROM sepsis.hadm_deliberation
-JOIN mimic.admissions USING (hadm_id)
+JOIN admissions USING (hadm_id)
 WHERE angus = 1
 UNION
 SELECT
 2 -- angus shock
 , mimic_id , admittime , dischtime
 FROM sepsis.hadm_deliberation
-JOIN mimic.admissions USING (hadm_id)
+JOIN admissions USING (hadm_id)
 WHERE angus = 2
 UNION
 SELECT
 3 -- accp_severe
 , mimic_id , admittime , dischtime
 FROM sepsis.hadm_deliberation
-JOIN mimic.admissions USING (hadm_id)
+JOIN admissions USING (hadm_id)
 WHERE accp = 1
 UNION
 SELECT
 4 -- accp_shock
 , mimic_id , admittime , dischtime
 FROM sepsis.hadm_deliberation
-JOIN mimic.admissions USING (hadm_id)
+JOIN admissions USING (hadm_id)
 WHERE accp = 2
 UNION
 SELECT
 5 -- sepsis severe
 , mimic_id , admittime , dischtime
 FROM sepsis.hadm_deliberation
-JOIN mimic.admissions USING (hadm_id)
+JOIN admissions USING (hadm_id)
 WHERE sepsis3 = 1
 UNION
 SELECT
 6 -- sepsis shock
 , mimic_id , admittime , dischtime
 FROM sepsis.hadm_deliberation
-JOIN mimic.admissions USING (hadm_id)
+JOIN admissions USING (hadm_id)
 WHERE sepsis3 = 2
