f2dfef05f742d3de2a06558364640603efe76659
Nicolas Paris <nicolas.paris@aphp.fr>
2018-01-21 13:58:52 +0100

    Fix sepsis derived variables

--- etl/StandardizedDerivedElements/COHORT/etl.sql
+++ etl/StandardizedDerivedElements/COHORT/etl.sql
@@ -1,3 +1,5 @@
+USE mimiciii;
+TRUNCATE TABE omop.cohort;
 INSERT INTO omop.cohort 
 (
   cohort_definition_id 
@@ -9,41 +11,41 @@ INSERT INTO omop.cohort
 SELECT
 1 -- angus severe
 , mimic_id , admittime , dischtime
-FROM sepsis.hadm_deliberation
-JOIN mimic.admissions USING (hadm_id)
+FROM sepsis.hadm_deliberation_all
+JOIN admissions USING (hadm_id)
 WHERE angus = 1
 UNION
 SELECT
 2 -- angus shock
 , mimic_id , admittime , dischtime
-FROM sepsis.hadm_deliberation
-JOIN mimic.admissions USING (hadm_id)
+FROM sepsis.hadm_deliberation_all
+JOIN admissions USING (hadm_id)
 WHERE angus = 2
 UNION
 SELECT
 3 -- accp_severe
 , mimic_id , admittime , dischtime
-FROM sepsis.hadm_deliberation
-JOIN mimic.admissions USING (hadm_id)
+FROM sepsis.hadm_deliberation_all
+JOIN admissions USING (hadm_id)
 WHERE accp = 1
 UNION
 SELECT
 4 -- accp_shock
 , mimic_id , admittime , dischtime
-FROM sepsis.hadm_deliberation
-JOIN mimic.admissions USING (hadm_id)
+FROM sepsis.hadm_deliberation_all
+JOIN admissions USING (hadm_id)
 WHERE accp = 2
 UNION
 SELECT
 5 -- sepsis severe
 , mimic_id , admittime , dischtime
-FROM sepsis.hadm_deliberation
-JOIN mimic.admissions USING (hadm_id)
+FROM sepsis.hadm_deliberation_all
+JOIN admissions USING (hadm_id)
 WHERE sepsis3 = 1
 UNION
 SELECT
 6 -- sepsis shock
 , mimic_id , admittime , dischtime
-FROM sepsis.hadm_deliberation
-JOIN mimic.admissions USING (hadm_id)
+FROM sepsis.hadm_deliberation_all
+JOIN admissions USING (hadm_id)
 WHERE sepsis3 = 2
