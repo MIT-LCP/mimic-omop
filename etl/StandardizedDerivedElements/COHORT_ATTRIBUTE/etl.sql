2ce9da8dad3c6ebc83265754e44c351a155fe7d6
parisni <nicolas.paris@riseup.net>
2018-01-30 22:13:41 +0100

    Improve cohort attribute descriptions

--- etl/StandardizedDerivedElements/COHORT_ATTRIBUTE/etl.sql
+++ etl/StandardizedDerivedElements/COHORT_ATTRIBUTE/etl.sql
@@ -2,4 +2,4 @@
 -- visit_detail
 
 -- Load the cohort attribute extracted by Alistair on sunday morning
-\copy omop.cohort_attribute FROM PROGRAM 'gzip -dc ~/git/mimic-omop/extra/private/cohort_attribute.csv.gz' CSV HEADER NULL '' QUOTE '"';
+\copy omop.cohort_attribute FROM PROGRAM 'gzip -dc ~/git/mimic-omop/extras/private/cohort_attribute.csv.gz' CSV HEADER NULL '' QUOTE '"';
