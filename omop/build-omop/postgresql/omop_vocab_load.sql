d5b0e57547e3780c18702b1084ca7441cc5c9139
Alistair Johnson <alistairewj@gmail.com>
2018-03-26 13:02:59 -0400

    remove schema set from file

--- omop/build-omop/postgresql/omop_vocab_load.sql
+++ omop/build-omop/postgresql/omop_vocab_load.sql
@@ -1,13 +1,13 @@
 /*********************************************************************************
 # Copyright 2014 Observational Health Data Sciences and Informatics
 #
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,14 +17,14 @@
 
 /************************
 
- ####### #     # ####### ######      #####  ######  #     #           ####### 
- #     # ##   ## #     # #     #    #     # #     # ##   ##    #    # #       
- #     # # # # # #     # #     #    #       #     # # # # #    #    # #       
- #     # #  #  # #     # ######     #       #     # #  #  #    #    # ######  
- #     # #     # #     # #          #       #     # #     #    #    #       # 
- #     # #     # #     # #          #     # #     # #     #     #  #  #     # 
- ####### #     # ####### #           #####  ######  #     #      ##    #####  
-                                                                              
+ ####### #     # ####### ######      #####  ######  #     #           #######
+ #     # ##   ## #     # #     #    #     # #     # ##   ##    #    # #
+ #     # # # # # #     # #     #    #       #     # # # # #    #    # #
+ #     # #  #  # #     # ######     #       #     # #  #  #    #    # ######
+ #     # #     # #     # #          #       #     # #     #    #    #       #
+ #     # #     # #     # #          #     # #     # #     #     #  #  #     #
+ ####### #     # ####### #           #####  ######  #     #      ##    #####
+
 
 Script to load the common data model, version 5.0 vocabulary tables for PostgreSQL database on Windows (MS-DOS style file paths)
 The database account running this script must have the "superuser" permission in the database.
@@ -32,7 +32,7 @@ The database account running this script must have the "superuser" permission in
 Notes
 
 1) There is no data file load for the SOURCE_TO_CONCEPT_MAP table because that table is deprecated in CDM version 5.0
-2) This script assumes the CDM version 5 vocabulary zip file has been unzipped into the "../../athena/" directory. 
+2) This script assumes the CDM version 5 vocabulary zip file has been unzipped into the "../../athena/" directory.
 3) If you unzipped your CDM version 5 vocabulary files into a different directory then replace all file paths below, with your directory path.
 4) Truncate each table that will be lodaed below, before running this script.
 
@@ -42,7 +42,6 @@ author:  Lee Evans
 
 
 *************************/
-set search_path to omop;
 BEGIN;
  --SET CONSTRAINTS ALL DEFERRED;
 TRUNCATE TABLE concept CASCADE;
@@ -65,4 +64,3 @@ TRUNCATE TABLE drug_strength CASCADE;
 \copy CONCEPT_RELATIONSHIP FROM 'extras/athena/CONCEPT_RELATIONSHIP.csv' WITH DELIMITER E'\t' CSV HEADER QUOTE E'\b' ;
 \copy DRUG_STRENGTH FROM 'extras/athena/DRUG_STRENGTH.csv' WITH DELIMITER E'\t' CSV HEADER QUOTE E'\b' ;
 COMMIT;
-
