3ee311eba228ceebd8903e38c841785e7c74d510
Alistair Johnson <alistairewj@gmail.com>
2018-03-26 13:17:00 -0400

    delete unused files

--- omop/build-omop/postgresql/build-omop.sql
+++
@@ -1,6 +0,0 @@
-DROP SCHEMA omop CASCADE;
-CREATE SCHEMA omop;
-SET search_path TO omop;
-\i 'OMOP CDM ddl - PostgreSQL.sql'
-\i 'OMOP CDM constraints - PostgreSQL.sql'
-\i 'OMOP CDM comments - PostgreSQL.sql'
