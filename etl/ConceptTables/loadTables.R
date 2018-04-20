f87bdf15463133d88fd635c344834805c66d104a
parisni <nicolas.paris@riseup.net>
2018-04-21 00:58:31 +0200

    Make loadConcept be verbose

--- etl/ConceptTables/loadTables.R
+++ etl/ConceptTables/loadTables.R
@@ -50,6 +50,7 @@ dbSendQuery(con, sql)
 PATH_CSV <- "extras/concept/"
 fichs <- list.files(PATH_CSV,pattern="*.csv")
 for(fich in fichs){
+	print(sprintf("Loaded %s", fich))
 	tmp <- readDf(file.path(PATH_CSV,fich))
 	names(tmp) <- tolower(names(tmp))
 	dropTable(fich, SCHEMA_TARGET)
