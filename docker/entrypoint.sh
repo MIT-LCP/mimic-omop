d7b3a40ca095cd897ad9e7a82776e622a4a3023c
Nicolas Paris <nicolas.paris@adevinta.com>
2024-03-17 08:30:23 +0100

    Improve doc, and make check be the last action

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,4 +3,4 @@ set -e # Exit immediately if a command exits with a non-zero status.
 
 cd /opt/mimic-omop/
 echo -e "dbname=mimic\nuser=postgres\nhost=db\nport=5432\npassword=$PGPASSWORD" > /opt/mimic-omop/mimic-omop.cfg
-make build runetl check exporter 
+make build runetl exporter check 
